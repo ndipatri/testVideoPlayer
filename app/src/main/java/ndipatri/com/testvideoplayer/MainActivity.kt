e4c46a98c4e49923a185b37bbc34391e758e66c1
Nicholas DiPatri <ndipatri@gmail.com>
2018-07-06 15:53:04 -0400

    no message

---
+++ app/src/main/java/ndipatri/com/testvideoplayer/MainActivity.kt
@@ -0,0 +1,50 @@
+package ndipatri.com.testvideoplayer
+
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.VideoView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        videoView.setVideoURI(Uri.parse("rtsp://ers.evostream.com:5544/stream2"))
+        videoView.setOnErrorListener({mediaPlayer: MediaPlayer, i: Int, i1: Int ->
+            Log.d("MainActivity", "Error in MediaPlayer: $mediaPlayer")
+        false})
+        videoView.start()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
